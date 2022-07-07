915165cc304107e0f741e053cfe9e56cecba6bbc
Duncan Peterson <dtpeterson2@madisoncollege.edu>
2022-07-07 16:21:52 -0500

    Strange errors be here

---
+++ week3/boolean.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+##auth: Duncan Peterson
+##Desc: This script does boolean stuff. My favorite. I loved discrete mathematics and FPGAs
--- week3/files.py
+++ week3/files.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 ##auth: Duncan Peterson
 ##Desc: This script showcases file read python capabilities
-#It also tests some things as practice for myself.
 import keyword
 
 print("\n") #break
@@ -11,13 +10,13 @@ print("\n") #break
 with open("/etc/passwd","r") as pw1:
     #pw1 represents the file. we can't do much with it yet.
     contents1 = pw1.read()
-    print(f"#File char length: {len(contents1)}")
+    print(f"File char length: {len(contents1)}")
     print("passwd now read as contents1. We can control contents1 however we please,\
         \n and write to a file later")
+    print(contents1)
     #This saved contents1 as the whole thing. We can close and do anything we want.
-    #DON'T DO THIS: print(contents1)
     #del contents1
-    pw1.close()
+    #pw1.close()    #Not needed because the file closes at the DEDENT
 
 print("\n") #break
 
@@ -29,10 +28,12 @@ with open("/etc/passwd","r") as pw2:
         \n and write to a file later. \n \
         It's perhaps easier to navigate large files by line, but we may lose \n \
         sense of relationship between columns doing it this way.")
-    pw2.close()
+    #pw2.close() #Not needed because the file closes at the DEDENT
 
 #3. Open passwd and save it to a variable line by line.
 
+#STOP THIS MADNESS. Use a for lines in pw3 loop.
+"""
 with open("/etc/passwd","r") as pw3:
     contents3="" #init
     while True:
@@ -49,3 +50,34 @@ with open("/etc/passwd","r") as pw3:
     print(type(contents3))
 
     pw3.close()
+"""
+print("\n") #break
+with open("/etc/passwd","r") as pw3:
+    contents3="" #init
+    pw3.seek(0,0)
+#I tried to count tell() deltas beacuse i'm insane, but apparently the pointer is abstracted 
+#within a 'for line in file:' loop. OSError; telling position disabled by next() call. An interesting fact.
+
+    for line in pw3:
+        contents3 = contents3 + pw3.readline()
+    print(f"File length: {len(contents3)}") #The sane way to do things.
+    print(contents3)
+
+    print("""I imagine we can use a for loop to check the whole file, and an if: statement
+    to check for a peculiarly formatted block. 
+    In the context of yanking the kcode block of my MCNP6.2 nuclear reactor simulation,
+    I would use an if statement to check for the following string on every line...
+    
+    \"1neutron  activity in each cell {73 white spaces} print table 126\"
+    
+    and then I will kcode_block=kcode_block + core0f_o.readline() 
+    while: I haven't reachedthe kcode terminator which consists of:
+    a whitespace, 131 dashes, a carriage return, and a line feed,
+    and then break.
+    
+    Capturing things in MCNP isn't too easy because it's all formatted for teletype machines.
+    Many of the terminators are just for the eyes and are not unique.
+    We can still count and check for back-to-back LFs, which is something MCNP does a lot
+    
+    :)""")
+
