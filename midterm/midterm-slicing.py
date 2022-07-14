fd8547bcb476afe880428675ad4ea2d7e1c83e12
Duncan Peterson <dtpeterson2@madisoncollege.edu>
2022-07-14 18:15:58 -0500

    This is slicing as it is. \n can't be .replace()'d for some reason.

--- midterm/midterm-slicing.py
+++ midterm/midterm-slicing.py
@@ -16,31 +16,27 @@ with open('midterm/slicing-file.txt',"r") as file:  #load the file as something 
 #4 Create variables for each item and use a SINGLE slice to get the correct words
 #4a the third word from the EOF
     fourA=str(f"{contents[-3:-4:-1]}")   #When you go negative, 0 is the EOF, and -1 is the last line before EOF.    #[-3:-4:-1]
-    print(fourA)
 #4b the 3rd through 5th words.
     fourB=f"{contents[2:5:1]}"  #Remember, the 2nd index is the 3rd line. Lists begin at 0, but not in reverse- then the 0th is the EOF and -1 is the last line
-    print(fourB)
 #4c The 10th word from the EOF, and every other word for a total of 3 words.
     fourC=f"{contents[-10:-15:-2]}"
-    print(fourC)
 #4d The 11th through 13th words.
     fourD=f"{contents[10:13:1]}"
-    print(fourD)
 #4e the 19th through 21st words from the EOF
     fourE=f"{contents[-19:-22:-1]}"
-    print(fourE)
     #print(contents[1])
     print("")   #blank line
 #5. Add each word to a new string. Some slices will return a list. Use join() to join the list into a single string
     #combined=str(' ').join([fourA, fourB, fourC, fourD, fourE])    #nice try, but doing this with a list results in some weird behavior.
     #You can only give join a single argument.
-    combined=str('').join([fourA, fourB, fourC, fourD, fourE])
-    print(combined)
-    print(type(combined))
-#6. Print the quote. you can use .replace() to remove end of line characters.
+    combined=str('').join([fourA])
     combined=combined.replace("[","")    #Delete these.
     combined=combined.replace("]","")    #Delete that.
-    combined=combined.replace("\n"," ")   #No more line feeds.
+    combined=combined.replace("\n"," ")   #No more line feeds... Why is this so stubborn?
     combined=combined.replace("'","")   #No single quotes.
+
+#6. Print the quote. you can use .replace() to remove end of line characters.
     print(combined)
+    #Why is the LF so stubborn?
+
 #7. Test your script. If you did everything right, you'll see: "Whether you think you can or you think you can't, you are right."
