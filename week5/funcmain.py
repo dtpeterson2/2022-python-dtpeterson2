07d5e282046da61297e4905062760361e433fa1c
Duncan Peterson <dtpeterson2@madisoncollege.edu>
2022-07-18 15:36:01 -0500

    Did some stuff.

--- week5/funcmain.py
+++ week5/funcmain.py
@@ -6,9 +6,17 @@
 import keyword
 None    #void main(). Hahahah C syntax
 def main():
-    print("__name__")
-    print("toot. You just ran main.")
-    return None
+    print(__name__)
+    print("argh")
+    print("Ran main()")
+#    print(args)
+    print("Done")
+    return int(1)
 
-if __name__ == "__main__":  #This is an awful thing Python does where, if you call the script directly, the code that runs is main(). This doesn't run if you 'import'. You have to write it this way.
+def create_tea(user_id,domain="teapot.edu"):    #You can't write 'user'. That's a special name.
+    return user_id + "@" + domain
+
+print("Funcmain line 18 got ran")   #A thingy to tell us that line 18 got executed.
+
+if __name__ == "__main__":  #This is an awful thing Python does where, if you call the script directly by name, the code that runs is main(). This doesn't run if you 'import'. You have to write it this way.
     main()
--- week5/functest.py
+++ week5/functest.py
@@ -18,16 +18,9 @@ def functest():
     var_1="I'm gonna collide with a global"
     print(var_1)
 
-functest()
-print("func done. Here's var_1")
-print(type(var_1))
-print(var_1)
-
 def multivar(arg1, arg2): #Functions aren't like scripts that can only take a single argv. We can have a lot of inputs with funcs.
     print(arg1)
     print(arg2)
-
-multivar("derp","herp")
 
 def allvars(*arg):  #This takes as many args as you want and assigns them as a list.
     for i in arg:
@@ -35,5 +28,14 @@ def allvars(*arg):  #This takes as many args as you want and assigns them as a l
     return print("wow")
 
 allvars("The","Quick","Brown","Fox")
+
+import funcmain #You can import scripts to gain the functionality of the defined functions within.
+
+email_address = funcmain.create_tea("Teapotmail")
+print(f"Email Address: {email_address}")
+#It turns out that when we call things from funcmain, funcmain runs in its entirety excluding any failed conditionals.
+
+#Lets try calling directly.
+callingmain = funcmain()    #I know it returns None, but lets see what happens.
 
 
